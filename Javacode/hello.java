d56d3a808637e7dc4ee2768589a8a5ee79320031
rahul9636 <50780541+rahul9636@users.noreply.github.com>
2021-06-17 11:40:40 +0530

    Update hello.java

--- Javacode/hello.java
+++ Javacode/hello.java
@@ -1,6 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
+        System.out.println("Hello world again");
     }
+    public void Greeting(String name){
+	System.out.println("hello world);
+    }
+
 }
 
